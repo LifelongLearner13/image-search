0529c08d0fd616a438b79520c00da34453732f7b
Sierra Gregg <sierragregg@gmail.com>
2017-09-16 19:42:14 -0500

    Load images from api

--- src/public/js/image_search.js
+++ src/public/js/image_search.js
@@ -1,5 +1,13 @@
 var app = angular.module('imageSearch', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
+app.controller('MainCtrl', function($scope, $http) {
+    $scope.imageSearch = function(query) {
+      query = query.trim().split(' ').join('+');
+      var apiBase = document.location.href + 'api/search/';
+      $http.get(apiBase + query).then(function(res) {
+          $scope.images = res.data.photos.photo;
+        }).catch(function(err) {
+          $scope.error = err;
+        });
+    };
 });
